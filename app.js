22d546f569e947300fb68ee20929ba80e09e4607
MIClavelTe <miclavelte@gmail.com>
2018-07-22 21:09:31 -0700

    Add message for error

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
+// const https = require('https');
+
+// function printMessage(user, badges, points) {
+//   const message = `${user} has ${badges} badge(s) and ${points} JS points`;
+//   console.log(message);
+// }
+
+// function getProfile(username) {
+//   try {
+//     const request = https.get(`https://teamtreehouse.co/${username}.json`, response => {
+//       let body = ""
+
+//       response.on('data', data => {
+//         body += data.toString();
+//       });
+  
+//       response.on('end', () => {
+//         var profile = JSON.parse(body);
+//         printMessage(profile.name, profile.badges.length, profile.points.JavaScript);
+//       });
+//     });
+//   } catch(error) {
+//     console.log('Error:' + error.message);
+//   }
+// }
+
+
+// const users = process.argv.slice(2);
+// users.forEach(getProfile);
+
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+//___________________________________________________________________________________________________________________________
+
 const https = require('https');
 
 function printMessage(user, badges, points) {
-    const message = `${user} has ${badges} badge(s) and ${points} JS points`;
-    console.log(message);
+  const message = `${user} has ${badges} badge(s) and ${points} JS points`;
+  console.log(message);
 }
 
 function getProfile(username) {
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-        let body = ""
+  try {
+    const request = https.get(`https://teamtreehouse./${username}.json`, response => {
+      let body = ""
+      
+      response.on('data', data => {
+        body += data.toString();
+      });
 
-        response.on('data', data => {
-            body += data.toString();
-        });
-
-        response.on('end', () => {
-            var profile = JSON.parse(body);
-            printMessage(profile.name, profile.badges.length, profile.points.JavaScript);
-        });
+      response.on('end', () => {
+        var profile = JSON.parse(body);  
+        console.log(username);                          
+        printMessage(profile.name, profile.badges.length, profile.points.JavaScript);
+      });                          
     });
-};
+    request.on('error', error => console.error(`Error: ${error.message}`));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 const users = process.argv.slice(2);
 users.forEach(getProfile);
