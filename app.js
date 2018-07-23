fec65c2d098af057c9e6148eb3d357c1f1415ac2
MIClavelTe <miclavelte@gmail.com>
2018-07-22 22:49:07 -0700

    Seperate into files

--- app.js
+++ app.js
@@ -1,44 +1,4 @@
-const https = require('https');
-
-function printMessage(user, badges, points) {
-  const message = `${user} has ${badges} badge(s) and ${points} JS points`;
-  console.log(message);
-}
-
-function printError(message) {
-  console.error(message);
-}
-
-function getProfile(username) {
-  try {
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-      if (response.statusCode === 200) {
-        let body = ""
-      
-        response.on('data', data => {
-          body += data.toString();
-        });
-
-        response.on('end', () => {
-          try {
-            var profile = JSON.parse(body);  
-            console.log(username);                          
-            printMessage(profile.name, profile.badges.length, profile.points.JavaScript);
-          } catch (error) {
-            console.error('Your input does not exist');
-          }
-        });  
-        
-      } else {
-        const statusError = `Status Code: ${response.statusCode}`;
-        printError(statusError);
-      }
-    });
-    request.on('error', error => printError('Problem with Request'));
-  } catch (error) {
-    printError('Problem with Request');
-  }
-}
+const profile = require('./profile.js');
 
 const users = process.argv.slice(2);
-users.forEach(getProfile);
+users.forEach(profile.getProfile);
